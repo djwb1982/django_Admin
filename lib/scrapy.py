e89fb78a7387a77b06c662b65bc791a55eb954dd
king1982 <djwb1982@163.com>
2017-03-28 23:39:00 +0800

    urllib lib

--- lib/scrapy.py
+++ lib/scrapy.py
@@ -10,7 +10,7 @@ header = {
             , "Connection": "keep-alive"
          }
 set = {
-    'site':{'home':'http://blog.csdn.net','url':'http://blog.csdn.net/winterto1990/article/details/47903653','xpath':'//*[@id="panel_Category"]/ul/li/a/@href'},
+    'site':{'home':'http://www.zngirls.com/rank/sum/','url':'http://www.zngirls.com/rank/sum/','xpath':'//div[@class="pagesYY"]/div/a/@href'},
     'lists':{'xpath':'//div[@class="rankli_imgdiv"]/a/@href'},
     'content':{'xpath':'//div[@class="rankli_imgdiv"]/a/@href'}
 }
@@ -31,16 +31,24 @@ class Scrapy:
            for (p, v) in enumerate(pages):
                url = self.set['site']['home']+v
                self.list[total+p] = url
-           print(self.list)
-       def Lists(self):
-           print(2222)
+       def Lists(self,listUrl):
+           request = urllib.request.Request(listUrl)
+           request.add_header('User-Agent', self.header['User-Agent'])
+           request.add_header('Connection', self.header['Connection'])
+           html = urllib.request.urlopen(request)
+           data = html.read()
+           path = etree.HTML(data)
+           pages = path.xpath(self.set['lists']['xpath'])
+           total = len(self.list)
+           for (p, v) in enumerate(pages):
+               url = self.set['site']['home'] + v
+               self.list[total + p] = url
        def Contents(self):
            print(333)
+
 sc=Scrapy(set,header)
 sc.Sites()
-PATH = os.path.dirname(os.path.realpath(__file__))
-logging.basicConfig(filename=PATH + '/crawl.log', filemode='a',
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(lineno)s - %(message)s',
-                    level=logging.INFO)
-logging.info('3333333')
+for k,v in sc.list.items():
+     sc.Lists(v)
+
 
---
+++ utils/myScrapy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+import urllib.request
+from lxml import etree
+from os import system
+pciturelist=[]
+
+header = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
+            , "Connection": "keep-alive"
+         }
+class myScrapy:
+    def list(self):
+        pass
+    def content(self):
+        pass
+my = myScrapy()
+my.list()
